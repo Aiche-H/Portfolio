3bffdfc80e249e0cebe1ae0bf09d0619f809fb49
Henrikki <henrikki.gren@hotmail.com>
2024-10-24 15:12:34 +0300

    added some more stylings also made it a bit more flashy

--- global/javascript/global.js
+++ global/javascript/global.js
@@ -1,5 +1,6 @@
 const MENU_BUTTON = document.getElementById("menuButton");
 const HEADER = document.querySelector("header");
+const BUTTONS = document.querySelectorAll("button");
 
 MENU_BUTTON.addEventListener("click", () => {
   HEADER.classList.toggle("hidden");
@@ -21,4 +22,29 @@ window.addEventListener("load", () => {
     HEADER.style.display = "none";
     MENU_BUTTON.style.position = "static";
   }
+});
+
+BUTTONS.forEach((button) => {
+  const buttonTextToType = button.textContent;
+  let typingIndex = 0;
+  let typingIntervalId = null;
+
+  button.addEventListener("mouseover", () => {
+    typingIndex = 0;
+    button.textContent = "";
+
+    typingIntervalId = setInterval(() => {
+      if (typingIndex < buttonTextToType.length) {
+        button.textContent += buttonTextToType[typingIndex];
+        typingIndex++;
+      } else {
+        clearInterval(typingIntervalId);
+      }
+    }, 50);
+  });
+
+  button.addEventListener("mouseout", () => {
+    clearInterval(typingIntervalId);
+    button.textContent = buttonTextToType;
+  });
 });
--- pages/index/local-javascript/index.js
+++ pages/index/local-javascript/index.js
@@ -2,10 +2,29 @@ const HEAD_IMAGE = document.querySelector(".profilePicture");
 const POPUP = document.createElement("div");
 POPUP.className = "popup";
 
+const textToType = "Click the image to close.";
+let typingIndex = 0;
+
 HEAD_IMAGE.addEventListener("click", () => {
-  POPUP.innerHTML = `<img src="${HEAD_IMAGE.src}" alt="Profile Picture">`;
+  // reset typingIndex
+  typingIndex = 0;
+
+  POPUP.innerHTML = `
+    <img src="${HEAD_IMAGE.src}" alt="Profile Picture">
+    <p id="typingParagraph"></p>
+  `;
   document.body.appendChild(POPUP);
   POPUP.style.display = "block";
+
+  const typingParagraph = document.getElementById("typingParagraph");
+  const typingInterval = setInterval(() => {
+    if (typingIndex < textToType.length) {
+      typingParagraph.textContent += textToType[typingIndex];
+      typingIndex++;
+    } else {
+      clearInterval(typingInterval);
+    }
+  }, 50);
 });
 
 document.addEventListener("click", (e) => {
